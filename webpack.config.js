1c3d61681e93895455c0e6a2f5d49e21f9242cb6
Scott j Sullivan <ssulivan11@gmail.com>
2016-08-23 16:01:53 -0500

    Add files via upload

--- webpack.config.js
+++ webpack.config.js
@@ -11,13 +11,12 @@ var config = {
     filename: 'bundle.js'
   },
   module : {
-    loaders : [
-      {
-        test : /\.jsx?/,
-        include : APP_DIR,
-        loader : 'babel'
-      }
-    ]
+    loaders : [{ 
+      test: /\.jsx?$/, // Match both .js and .jsx files
+      exclude: /node_modules/, 
+      loader: "babel", 
+      query: {  presets:['es2015','react'] }
+    }]
   }
 };
 
